05d03c01d16697808175f0f05738528e5b3292dd
wsehl <widesehl@gmail.com>
2020-11-12 16:14:38 +0600

    minor changes 2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -102,7 +102,7 @@ module.exports = {
   ],
   stylesheets: [
     {
-      href: "katex/katex.min.css",
+      href: "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css",
       type: "text/css",
       integrity:
         "sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X",
