bbe058db1fc1e047f69d8d89b8b1747865d0149e
wsehl <widesehl@gmail.com>
2020-11-26 12:54:47 +0600

    -

--- src/pages/glossary.js
+++ src/pages/glossary.js
@@ -4,7 +4,7 @@ import Iframe from "@trendmicro/react-iframe";
 
 function App() {
   return (
-    <Layout title="Chemical Glossary App">
+    <Layout className="layout-body" title="Chemical Glossary App">
       <Iframe
         src="https://cie-ebook-glossary-app.netlify.app"
         width="100%"
@@ -14,30 +14,4 @@ function App() {
   );
 }
 
-function Hello() {
-  return (
-    <Layout title="Chemical Glossary App">
-      <Iframe
-        src="src/pages/app.html"
-        onLoad={({ event, iframe }) => {
-          if (!(iframe && iframe.contentDocument)) {
-            return;
-          }
-
-          const target = iframe.contentDocument.body;
-          const nextHeight = target.offsetHeight;
-          iframe.style.height = `${nextHeight}px`;
-
-          const observer = new ResizeObserver((entries) => {
-            const target = iframe.contentDocument.body;
-            const nextHeight = target.offsetHeight;
-            iframe.style.height = `${nextHeight}px`;
-          });
-          observer.observe(target);
-        }}
-      />
-    </Layout>
-  );
-}
-
-export default Hello;
+export default App;
