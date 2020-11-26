cf80fcf2a0bd354b19fcddfd7ce383a3ed805cb8
wsehl <widesehl@gmail.com>
2020-11-26 11:42:24 +0600

    local media

--- src/pages/glossary.js
+++ src/pages/glossary.js
@@ -14,4 +14,30 @@ function App() {
   );
 }
 
-export default App;
+function Hello() {
+  return (
+    <Layout title="Chemical Glossary App">
+      <Iframe
+        src="src/pages/app.html"
+        onLoad={({ event, iframe }) => {
+          if (!(iframe && iframe.contentDocument)) {
+            return;
+          }
+
+          const target = iframe.contentDocument.body;
+          const nextHeight = target.offsetHeight;
+          iframe.style.height = `${nextHeight}px`;
+
+          const observer = new ResizeObserver((entries) => {
+            const target = iframe.contentDocument.body;
+            const nextHeight = target.offsetHeight;
+            iframe.style.height = `${nextHeight}px`;
+          });
+          observer.observe(target);
+        }}
+      />
+    </Layout>
+  );
+}
+
+export default Hello;
