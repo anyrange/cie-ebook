15d430daa8077a35e50c792cde4dc4f7f543051a
wsehl <widesehl@gmail.com>
2020-12-07 21:19:42 +0600

    AudioButton0.5

--- src/components/AudioButton/index.js
+++ src/components/AudioButton/index.js
@@ -1,37 +1,29 @@
-/*
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
+import React, { useState, useRef } from "react";
 import "./AudioButton.css";
 
-const useAudio = (url) => {
-  const [audio] = useState(new Audio(url));
-  const [playing, setPlaying] = useState(false);
-
-  const toggle = () => setPlaying(!playing);
-
-  useEffect(() => {
-    playing ? audio.play() : audio.pause();
-  }, [playing]);
-
-  useEffect(() => {
-    audio.addEventListener("ended", () => setPlaying(false));
-    return () => {
-      audio.removeEventListener("ended", () => setPlaying(false));
-    };
-  }, []);
-
-  return [playing, toggle];
-};
-
 const AudioButton = ({ url }) => {
-  const [playing, toggle] = useAudio(url);
+  const [audioStatus, changeAudioStatus] = useState(false);
+  const myRef = useRef();
+
+  const startAudio = () => {
+    myRef.current.play();
+    changeAudioStatus(true);
+  };
+
+  const pauseAudio = () => {
+    myRef.current.pause();
+    changeAudioStatus(false);
+  };
+
   return (
-    <div
-      onClick={toggle}
-      className={playing ? "btn play-btn" : "btn pause-btn"}
-    ></div>
+    <>
+      <audio ref={myRef} src={url} />
+      <div
+        onClick={audioStatus ? pauseAudio : startAudio}
+        className={audioStatus ? "btn play-btn" : "btn pause-btn"}
+      ></div>
+    </>
   );
 };
 
 export default AudioButton;
-*/
