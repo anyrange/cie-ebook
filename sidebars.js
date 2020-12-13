220b6292df8555405d8d5c24f9224a24f4f3943c
thaseyor <56724794+thaseyor@users.noreply.github.com>
2020-12-13 23:19:19 +0600

    🐵 big atomic structure update

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,18 @@
 module.exports = {
   someSidebar: {
     Home: ["introduction", "in-dev"],
+    'Atomic Structure': [
+      "atomic structure/classroom-language",
+      "atomic structure/language-structure",
+      "atomic structure/vocabulary-practice-tips",
+      "atomic structure/glossary",
+      "atomic structure/reading",
+      "atomic structure/practice-exercises",
+      "atomic structure/bloom-taxonomy",
+      "atomic structure/video-lesson",
+      "atomic structure/students-self-study",
+      "atomic structure/teaching-method"
+    ],
     Periodicity: [
       "periodicity/classroom-language",
       "periodicity/language-structure",
