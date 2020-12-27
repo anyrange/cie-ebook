e1a6243c07db7f133d3e7057eb6a8a3c1328c087
wsehl <widesehl@gmail.com>
2020-12-27 18:54:36 +0600

    Revert "update"
    This reverts commit 54cb54ff35591231c8e8c98916926642210513e9.

--- src/components/Frame/index.js
+++ src/components/Frame/index.js
@@ -1,24 +1,6 @@
 import React from "react";
 
-const Frame = ({ src, width, height, scrolling, style, title, id }) => {
-  var eventMethod = window.addEventListener
-    ? "addEventListener"
-    : "attachEvent";
-  var eventer = window[eventMethod];
-  var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
-  eventer(
-    messageEvent,
-    function (e) {
-      // If the message is a resize frame request
-      if (e.data.indexOf("resize::") != -1) {
-        var height = e.data.replace("resize::", "");
-        document.querySelectorAll("iframe[data-src]").style.height = height + "px";
-        /* document.getElementById({ id }).style.height = height + "px"; */
-      }
-    },
-    false
-  );
-
+const Frame = ({ src, width, height, scrolling, style, title }) => {
   return (
     <iframe
       title={title}
@@ -27,9 +9,6 @@ const Frame = ({ src, width, height, scrolling, style, title, id }) => {
       height={height}
       scrolling={scrolling}
       style={style}
-      id={id}
-      frameborder="0"
-      loading="lazy"
     />
   );
 };
