54cb54ff35591231c8e8c98916926642210513e9
wsehl <widesehl@gmail.com>
2020-12-27 18:36:02 +0600

    update

--- src/components/Frame/index.js
+++ src/components/Frame/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 
-const Frame = ({ src, width, height, scrolling, style, title }) => {
+const Frame = ({ src, width, height, scrolling, style, title, id }) => {
+  var eventMethod = window.addEventListener
+    ? "addEventListener"
+    : "attachEvent";
+  var eventer = window[eventMethod];
+  var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+  eventer(
+    messageEvent,
+    function (e) {
+      // If the message is a resize frame request
+      if (e.data.indexOf("resize::") != -1) {
+        var height = e.data.replace("resize::", "");
+        document.querySelectorAll("iframe[data-src]").style.height = height + "px";
+        /* document.getElementById({ id }).style.height = height + "px"; */
+      }
+    },
+    false
+  );
+
   return (
     <iframe
       title={title}
@@ -9,6 +27,9 @@ const Frame = ({ src, width, height, scrolling, style, title }) => {
       height={height}
       scrolling={scrolling}
       style={style}
+      id={id}
+      frameborder="0"
+      loading="lazy"
     />
   );
 };
