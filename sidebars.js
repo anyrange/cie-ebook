989b5292a1975142883b29b5a1239a5fa019e67e
thaseyor <56724794+thaseyor@users.noreply.github.com>
2020-12-30 16:38:09 +0600

    💡 new features

--- sidebars.js
+++ sidebars.js
@@ -1,85 +1,88 @@
 module.exports = {
   someSidebar: {
-    Home: ["introduction", "about", "in-dev"],
-    "Atomic Structure": [
-      "atomic-structure/classroom-language",
-      "atomic-structure/language-structure",
-      "atomic-structure/vocabulary-practice-tips",
-      "atomic-structure/glossary",
-      "atomic-structure/reading-the-structure-of-an-atom", //task
-      "atomic-structure/reading-electron-shell-of-an-atom", //task
-      "atomic-structure/reading-mass-spectrometry", //task
-      "atomic-structure/video-lesson", //task
-      "atomic-structure/bloom-taxonomy",
-      "atomic-structure/students-self-study",
-      "atomic-structure/teaching-method",
+    Home: ['introduction', 'about', 'in-dev'],
+    'Atomic Structure': [
+      'atomic-structure/classroom-language',
+      'atomic-structure/language-structure',
+      'atomic-structure/vocabulary-practice-tips',
+      'atomic-structure/glossary',
+      'atomic-structure/reading-the-structure-of-an-atom', //task
+      'atomic-structure/reading-electron-shell-of-an-atom', //task
+      'atomic-structure/reading-mass-spectrometry', //task
+      'atomic-structure/video-lesson', //task
+      'atomic-structure/bloom-taxonomy',
+      'atomic-structure/students-self-study',
+      'atomic-structure/teaching-method',
     ],
     Periodicity: [
-      "periodicity/classroom-language",
-      "periodicity/language-structure",
-      "periodicity/vocabulary-practice-tips",
-      "periodicity/glossary",
-      "periodicity/reading-periodicity", //task
-      "periodicity/video-lesson", //task
-      "periodicity/bloom-taxonomy",
-      "periodicity/students-self-study",
-      "periodicity/teaching-method",
+      'periodicity/classroom-language',
+      'periodicity/language-structure',
+      'periodicity/vocabulary-practice-tips',
+      'periodicity/glossary',
+      'periodicity/reading-periodicity', //task
+      'periodicity/video-lesson', //task
+      'periodicity/bloom-taxonomy',
+      'periodicity/students-self-study',
+      'periodicity/teaching-method',
     ],
     Bonding: [
-      "bonding/classroom-language",
-      "bonding/language-structure",
-      "bonding/vocabulary-practice-tips",
-      "bonding/glossary",
-      "bonding/reading-bonding", //task
-      "bonding/video-lesson", //task
-      "bonding/bloom-taxonomy",
-      "bonding/students-self-study",
-      "bonding/teaching-method",
+      'bonding/classroom-language',
+      'bonding/language-structure',
+      'bonding/vocabulary-practice-tips',
+      'bonding/glossary',
+      'bonding/reading-bonding', //task
+      'bonding/video-lesson', //task
+      'bonding/bloom-taxonomy',
+      'bonding/students-self-study',
+      'bonding/teaching-method',
     ],
     Energetics: [
-      "energetics/classroom-language",
-      "energetics/language-structure",
-      "energetics/vocabulary-practice-tips",
-      "energetics/glossary",
-      "energetics/reading-the-enthalpy-changes", //task
-      "energetics/video-lesson", //task
-      "energetics/bloom-taxonomy",
-      "energetics/students-self-study",
-      "energetics/teaching-method",
+      'energetics/classroom-language',
+      'energetics/language-structure',
+      'energetics/vocabulary-practice-tips',
+      'energetics/glossary',
+      'energetics/reading-the-enthalpy-changes', //task
+      'energetics/video-lesson', //task
+      'energetics/bloom-taxonomy',
+      'energetics/students-self-study',
+      'energetics/teaching-method',
     ],
     Kinetics: [
-      "kinetics/classroom-language",
-      "kinetics/language-structure",
-      "kinetics/vocabulary-practice-tips",
-      "kinetics/glossary",
-      "kinetics/reading-the-rate-of-reaction", //task
-      "kinetics/video-lesson", //task
-      "kinetics/bloom-taxonomy",
-      "kinetics/students-self-study",
-      "kinetics/teaching-method",
+      'kinetics/classroom-language',
+      'kinetics/language-structure',
+      'kinetics/vocabulary-practice-tips',
+      'kinetics/glossary',
+      'kinetics/reading-the-rate-of-reaction', //task
+      'kinetics/video-lesson', //task
+      'kinetics/bloom-taxonomy',
+      'kinetics/students-self-study',
+      'kinetics/teaching-method',
     ],
     Equilibrium: [
-      "equilibrium/classroom-language",
-      "equilibrium/language-structure",
-      "equilibrium/vocabulary-practice-tips",
-      "equilibrium/glossary",
-      "equilibrium/reading-equilibrium",
-      "equilibrium/video-lesson",
-      "equilibrium/bloom-taxonomy",
-      "equilibrium/students-self-study",
-      "equilibrium/teaching-method",
+      'equilibrium/classroom-language',
+      'equilibrium/language-structure',
+      'equilibrium/vocabulary-practice-tips',
+      'equilibrium/glossary',
+      'equilibrium/reading-equilibrium',
+      'equilibrium/video-lesson',
+      'equilibrium/bloom-taxonomy',
+      'equilibrium/students-self-study',
+      'equilibrium/teaching-method',
     ],
-    Alkanes:[
-      "alkanes/classroom-language",
-      "alkanes/language-structure",
-      "alkanes/vocabulary-practice-tips",
-      "alkanes/glossary",
-      "alkanes/reading-alkanes",
-      "alkanes/reading-hydrocarbons",
-      "alkanes/video-lesson",
-      "alkanes/bloom-taxonomy",
-      "alkanes/students-self-study",
-      "alkanes/teaching-method",
-    ]
+    Alkanes: [
+      'alkanes/classroom-language',
+      'alkanes/language-structure',
+      'alkanes/vocabulary-practice-tips',
+      'alkanes/glossary',
+      'alkanes/reading-alkanes',
+      'alkanes/reading-hydrocarbons',
+      'alkanes/video-lesson',
+      'alkanes/bloom-taxonomy',
+      'alkanes/students-self-study',
+      'alkanes/teaching-method',
+    ],
+    Appendix: [
+      'appendix/appendix-1'
+    ],
   },
 };
