fd1740e5cff638795aef572f4c8a56194c0fc9e5
wsehl <widesehl@gmail.com>
2021-01-16 21:50:09 +0600

    💡 pwa

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,6 +75,62 @@ module.exports = {
         language: ["en", "ru"],
       },
     ],
+    [
+      "@docusaurus/plugin-pwa",
+      {
+        debug: true,
+        offlineModeActivationStrategies: ["appInstalled", "queryString"],
+        //swCustom: path.resolve(__dirname, 'src/sw.js'),
+        pwaHead: [
+          {
+            tagName: "link",
+            rel: "icon",
+            href: "/img/icon.png",
+          },
+          {
+            tagName: "link",
+            rel: "manifest",
+            href: "/manifest.json", // your PWA manifest
+          },
+          {
+            tagName: "meta",
+            name: "theme-color",
+            content: "rgb(37, 194, 160)",
+          },
+          {
+            tagName: "meta",
+            name: "apple-mobile-web-app-capable",
+            content: "yes",
+          },
+          {
+            tagName: "meta",
+            name: "apple-mobile-web-app-status-bar-style",
+            content: "#3a57ab",
+          },
+          {
+            tagName: "link",
+            rel: "apple-touch-icon",
+            href: "/img/icon.png",
+          },
+          {
+            tagName: "link",
+            rel: "mask-icon",
+            href: "/img/logo.svg",
+            color: "#3a57ab",
+          },
+          {
+            tagName: "meta",
+            name: "msapplication-TileImage",
+            content: "/img/icon.png",
+          },
+          {
+            tagName: "meta",
+            name: "msapplication-TileColor",
+            content: "#3a57ab",
+          },
+        ],
+      },
+    ],
   ],
   presets: [
     [
